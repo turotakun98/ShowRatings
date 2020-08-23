fdef7d66a54d9b3ebd7572d3f7ae6f210033279e
turotakun98 <davideturini98@gmail.com>
2020-08-23 17:12:45 +0200

    Bugfix misspelled span

--- src/components/loadingSpinner.jsx
+++ src/components/loadingSpinner.jsx
@@ -2,20 +2,20 @@ import React, { Component } from "react";
 import "./loadingSpinner.css";
 
 class LoadingSpinner extends Component {
-  render() {
-    if (this.props.loading) {
-      return (
-        <div className="lds-container">
-          <div className="lds-ring">
-            <div />
-            <div />
-            <div />
-            <div />
-          </div>
-        </div>
-      );
-    } else return <spans />;
-  }
+    render() {
+        if (this.props.loading) {
+            return (
+                <div className="lds-container">
+                    <div className="lds-ring">
+                        <div />
+                        <div />
+                        <div />
+                        <div />
+                    </div>
+                </div>
+            );
+        } else return <span />;
+    }
 }
 
 export default LoadingSpinner;
