b8c2ec603ec5848bcc68f0cc5a34f26e8e6cf024
turotakun98 <davideturini98@gmail.com>
2020-09-03 19:56:11 +0200

    Updates

---
+++ precache-manifest.28c4c47217056b7505acd3463a0ef076.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53cf7d63bb20b8e9a082eda3fc5447cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d3223a5d3173ce0e781",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "7a7bce3f5e31a1a6e19b",
+    "url": "/static/css/main.b212c67e.chunk.css"
+  },
+  {
+    "revision": "0d3223a5d3173ce0e781",
+    "url": "/static/js/2.b920b287.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b920b287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7bce3f5e31a1a6e19b",
+    "url": "/static/js/main.e74cb53d.chunk.js"
+  },
+  {
+    "revision": "12098e5c3896b86c31ef",
+    "url": "/static/js/runtime-main.7bf47a95.js"
+  },
+  {
+    "revision": "f15f494325a573281db9ab3d690c8bf7",
+    "url": "/static/media/iconImageNotFound.f15f4943.png"
+  }
+]);
